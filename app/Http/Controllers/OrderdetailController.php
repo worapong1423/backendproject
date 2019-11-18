48d91f85fa03c75958ab0f5b010608423b0c0f88
worapong1423 <d4rkn3s4@email.com>
2019-11-18 21:59:23 +0700

    add orderdetail

---
+++ app/Http/Controllers/OrderdetailController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Service\OrderdetailService;
+use App\Orderdetail;
+use Illuminate\Http\Request;
+
+class OrderdetailController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->service = new OrderdetailService();
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index($orderId)
+    {
+        //
+        return $this->service->getOrderdetail($orderId);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request,$orderId)
+    {
+        //
+        return $this->service->saveOrderdetail($request->all(),$orderId);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ app/Orderdetail.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Orderdetail extends Model
+{
+    //
+    protected $table = 'order_detail';
+    protected $fillable =[
+        'rate',
+        'amountin',
+        'amountout',
+    ];
+    public  function order(){
+        return $this->belongsTo(Order::class);
+    }
+    public  function rate(){
+        return $this->belongsTo(Rate::class,'product_id');
+    }
+}
